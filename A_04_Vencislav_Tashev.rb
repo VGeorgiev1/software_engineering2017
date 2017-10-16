3f13a3d5af5b6c88494d46be1dc12de478aa4178
wencakisa <wencakisa@gmail.com>
2017-10-16 18:36:46 +0300

    Added data validation and helper functions

--- A_04_Vencislav_Tashev.rb
+++ A_04_Vencislav_Tashev.rb
@@ -1,35 +1,60 @@
 require 'csv'
+require 'time'
 
-def is_heroku_url(url)
-  url.end_with? 'herokuapp.com/'
+def is_heroku_url?(url_string)
+  url_string.end_with?('herokuapp.com/')
 end
 
+def is_on_time?(current_date, max_date)
+  current_date <= max_date
+end
+
+def get_actual_results(url_string, actions)
+  initial_curl_request = "curl -s --form \"file=@/home/wencakisa/Downloads/mock_data.csv\""
+  actual_results = []
+
+  actions.each do |action|
+    request_url = url_string + action
+    actual_result = `#{initial_curl_request} #{request_url}`.to_s
+    actual_results.push actual_result
+  end
+
+  actual_results
+end
+
+def get_user_representation(user_info_hash)
+  user_class = user_info_hash[1].strip.upcase
+  user_number = user_info_hash[2]
+  user_full_name = "#{user_info_hash[3].strip} #{user_info_hash[4].strip}"
+
+  "#{user_full_name} (#{user_class}, #{user_number})"
+end
+
+date_format = '%d/%m/%Y %H:%M:%S'
+deadline = DateTime.strptime '10/10/2017 23:59:59', date_format
+
+actions = %w(sums filters intervals lin_regressions)
+expected = %w(51068.00 24758.00 1930.00 -0.000996,51.566565)
+
 CSV.foreach(ARGV[0], headers: true) do |row|
-  uri_string = row[5]
+  url_string = row[5]
 
-  if uri_string
-    uri_string = uri_string.to_s.strip
+  unless url_string.nil?
+    user_date = DateTime.strptime row[0], date_format
+    user_info = get_user_representation row
 
-    unless uri_string.end_with? '/'
-      uri_string << '/'
-    end
+    if is_on_time? user_date, deadline
+      url_string = url_string.strip
+      url_string << '/' unless url_string.end_with?('/')
 
-    if is_heroku_url(uri_string)
-      sums_url = uri_string << 'sums'
-      sums_expected = '51068.00'
-      sums_actual = `curl --form "file=@/home/wencakisa/Downloads/mock_data.csv" #{sums_url}`.to_s
+      if is_heroku_url? url_string
+        actual = get_actual_results url_string, actions
+        result = expected == actual ? 1 : 0
 
-      filters_url = uri_string << 'filters'
-      filters_expected = '51068.00'
-      filters_actual = `curl --form "file=@/home/wencakisa/Downloads/mock_data.csv" #{filters_url}`.to_s
-
-      intervals_url = uri_string << 'intervals'
-      intervals_expected = '51068.00'
-      intervals_actual = `curl --form "file=@/home/wencakisa/Downloads/mock_data.csv" #{intervals_url}`.to_s
-
-      lin_regressions_url = uri_string << 'lin_regressions'
-      lin_regressions_expected = '51068.00'
-      lin_regressions_actual = `curl --form "file=@/home/wencakisa/Downloads/mock_data.csv" #{lin_regressions_url}`.to_s
+        puts "#{user_info} has #{result}"
+      end
+    else
+      puts "#{user_info} is late."
     end
   end
 end
