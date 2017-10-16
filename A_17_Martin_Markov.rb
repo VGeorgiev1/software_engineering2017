55e348f4d7e282590d30f8b0fca8eac31d27fb9f
matir8 <martin.markov28@gmail.com>
2017-10-16 13:58:58 +0300

    Partialy done

--- A_17_Martin_Markov.rb
+++ A_17_Martin_Markov.rb
@@ -4,5 +4,10 @@ csv_file_path = './A_17_Martin_Markov.csv'
 hws_file_path = ARGV[0]
 
 CSV.foreach(hws_file_path) do |row|
-  puts "Time: #{row[0]} | Class: #{row[1]} | Number: #{row[2]} | First Name: #{row[3]} | Last Name: #{row[4]}"
+  print "#{row[3]} #{row[4]} #{row[1]} "
+  print `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/sums` == '55.00' &&
+        `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/filters` == '25.00' &&
+        `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/intervals` == '0.00' &&
+        `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/filters` == '1.000000,0.000000'
+  print "\n"
 end
