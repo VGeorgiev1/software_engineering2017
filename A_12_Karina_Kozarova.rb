c2c4bc3b4802ba364a3664775e29712c8b71ac07
Karina Kozarova <karinakozarova1303@gmail.com>
2017-10-16 19:01:18 +0300

    Update A_12_Karina_Kozarova.rb

--- A_12_Karina_Kozarova.rb
+++ A_12_Karina_Kozarova.rb
@@ -8,10 +8,10 @@ CSV.foreach(input_file) do |row|
 	current_url = row[5]
 
 	puts " Current: #{current_url} #{row[3]} #{row[4]}\n" 
-	r1 = `curl -F \"file=@/A_12_Karina_Kozarova.csv\" #{current_url}/sums` # 90.00
-    r2 = `curl -F \"file=@/A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  # 0.003953,1.913043
-	r3 = `curl -F \"file=@/A_12_Karina_Kozarova.csv\" #{current_url}/filters` # 150.00
-	r4 = `curl -F \"file=@/A_12_Karina_Kozarova.csv\" #{current_url}/intervals` #85.00
+	r1 = `curl -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/sums` # 90.00
+   	r2 = `curl -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/lin_regressions`  # 0.003953,1.913043
+	r3 = `curl -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/filters` # 150.00
+	r4 = `curl -F \"file=@./A_12_Karina_Kozarova.csv\" #{current_url}/intervals` #85.00
 
 
 	if r1 == "90.00" && r2== "0.003953,1.913043" && r3 == "150.00" && r4 == "85.00"
