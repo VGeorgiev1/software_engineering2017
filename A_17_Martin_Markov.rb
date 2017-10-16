6b93913f061ccb9294895f3c9c8f311964429070
matir8 <martin.markov28@gmail.com>
2017-10-16 18:19:53 +0300

    Done

--- A_17_Martin_Markov.rb
+++ A_17_Martin_Markov.rb
@@ -1,13 +1,21 @@
 require 'csv'
+require 'uri'
 
 csv_file_path = './A_17_Martin_Markov.csv'
 hws_file_path = ARGV[0]
 
 CSV.foreach(hws_file_path) do |row|
   print "#{row[3]} #{row[4]} #{row[1]} "
-  print `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/sums` == '55.00' &&
-        `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/filters` == '25.00' &&
-        `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/intervals` == '0.00' &&
-        `curl -s -F "file=@A_17_Martin_Markov.csv" #{row[5]}/filters` == '1.000000,0.000000'
+  begin
+    row[5].strip!
+    heroku_url = URI(row[5]).host
+  rescue
+    print "Invalid URL ->#{row[5]}\n"
+    next
+  end
+  print `curl -s -F "file=@A_17_Martin_Markov.csv" #{heroku_url}/sums` == '1275.00' &&
+        `curl -s -F "file=@A_17_Martin_Markov.csv" #{heroku_url}/filters` == '625.00' &&
+        `curl -s -F "file=@A_17_Martin_Markov.csv" #{heroku_url}/intervals` == '1065.00' &&
+        `curl -s -F "file=@A_17_Martin_Markov.csv" #{heroku_url}/lin_regressions` == '1.000000,0.000000' ? 1 : 0
   print "\n"
 end
