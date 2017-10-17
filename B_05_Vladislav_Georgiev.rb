59fced9c61de66e42cebb1a9169383a31e909c53
VGeorgiev1 <vladdislav.georgiev@gmail.com>
2017-10-17 23:43:55 +0300

    all done

--- B_05_Vladislav_Georgiev.rb
+++ B_05_Vladislav_Georgiev.rb
@@ -1,21 +1,37 @@
 require 'csv'
-
+require 'date'
 file=CSV.read(ARGV[0])
-
-correctSum=528
-correctFilter=272.00
-correctInterval=525
-correctRegression="1.000000,0.000000"
-file.each do |row| 
-    if row[1]!="" && row[2]!="" && row[3]!="" && row[4]!="" && row[5]!="" && row[6]!=""     
-        sum = `curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/sums`
-        filter =`curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/filters`
-        interval =`curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/intervals`
-        regression = `curl –form "file=@/home/elsyser/testing.csv" #{row[5]}/lin_regressions`
-        if sum==correctSum && filter==correctFilter && interval == correctInterval && regression==correctRegression
-            p "#{row[3]} #{row[4]}: 1" 
-        end
-    else 
-        p "#{row[3]} #{row[4]}: 0"
+count=1
+endDate=Date.parse('2017-10-10');
+file.drop(1).each do |row|
+	res=0
+	sum=''
+	filter=''
+	interval=''
+	regression=''
+	if row[5]!="" && row[4]!="" && row[3]!="" && row[2]!="" && row[1]!="" && row[5]!=nil && row[4]!=nil && row[3]!=nil && row[2]!=nil && row[1]!=nil
+		date=Date.parse(row[0].split(' ')[0]);	
+		if date > endDate
+			p "#{count}.#{row[3]} #{row[4]} => 0 (After the deadline #{row[0].split(' ')[0]})"
+		else
+		if row[5][-1]=="/"
+			sum= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}sums --max-time 5 2>/dev/null`
+			filter= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}filters --max-time 5 2>/dev/null`
+			interval= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}intervals --max-time 5 2>/dev/null`
+			regression= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}lin_regressions --max-time 5 2>/dev/null`
+				
+		else
+			sum= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}/sums --max-time 5 2>/dev/null`
+			filter= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}/filters --max-time 5 2>/dev/null`
+			interval= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}/intervals --max-time 5 2>/dev/null`
+			regression= `curl --form 'file=@/home/vladislav/testing.csv' #{row[5]}/lin_regressions --max-time 5 2>/dev/null`
+		end 
+		if sum=="528.00" && filter=="272.00" && interval=="525.00" && regression=="1.000000,0.000000"
+        		p "#{count}.#{row[3]} #{row[4]} => 1"
+		else
+			p "#{count}.#{row[3]} #{row[4]} => 0"
+		end
+		end	
+		count+=1
     end
-end 
+end
